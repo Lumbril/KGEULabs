7b1520b169ee3339d68c4c752fb54413a907ff2d
Lumbril <32036622+Lumbril@users.noreply.github.com>
2020-10-29 13:58:26 +0300

    added lab 7 v1 for Alex

--- Alex/Lab6/main.cpp
+++ Alex/Lab6/main.cpp
@@ -13,10 +13,10 @@ vector<vector <double>> M;  //матрица миноров
 void inputArray() {
     int k = -1;
     int val = 1;
-    
+
     for (int i = 0; i < A.size(); i++) {
         k *= -1;
-        
+
         if (k > 0) {
             for (int j = 0; j < A[i].size(); j += k){
                 A[j][i] = val++;
@@ -40,19 +40,19 @@ void copyArray() {
 //Транспонирование матрицы AT
 void T() {
     vector<vector <double>> buff;
-    
+
     buff.resize(AT.size());
-    
+
     for (int i = 0; i < AT.size(); i++) {
         buff[i].resize(AT[i].size());
     }
-    
+
     for (int i = 0; i < AT.size(); i++) {
         for (int j = 0; j < AT[i].size(); j++) {
             buff[j][i] = AT[i][j];
         }
     }
-    
+
     for (int i = 0; i < AT.size(); i++) {
         for (int j = 0; j < AT[i].size(); j++) {
             AT[i][j] = buff[i][j];
@@ -63,33 +63,33 @@ void T() {
 //Нахождение определителя методом Гаусса
 double searchDet() {
     double det = 1;
-    
+
     for (int i = 0; i < B.size(); i++) {
         int k = i;
-        
+
         for (int j = i + 1; j < B[i].size(); j++) {
             if (abs(B[j][i]) > abs(B[k][i])) {
                 k == j;
             }
         }
-        
+
         if (abs(B[k][i]) < EPS) {
 		    det = 0;
 		    break;
 	    }
-	    
+
 	    swap(B[i], B[k]);
-	    
+
 	    if (i != k) {
 	        det = -det;
 	    }
-	    
+
 	    det *= B[i][i];
-	    
+
 	    for (int j = i + 1; j < B[i].size(); j++) {
 	        B[i][j] /= B[i][i];
 	    }
-	    
+
 	    for (int j = 0; j < B[i].size(); j++) {
 	        if (j != i && abs(B[j][i]) > EPS) {
 	            for (int k = i + 1; k < B[i].size(); k++) {
@@ -98,7 +98,7 @@ double searchDet() {
 	        }
 	    }
     }
-    
+
     return det;
 }
 
@@ -107,12 +107,12 @@ void multMatrix() {
     for (int i = 0; i < AT.size(); i++) {
         for (int j = 0; j < AT[i].size(); j++) {
             double sum = 0;
-            
+
             for (int k = 0; k < AT.size(); k++) {
                 sum += A[i][k] * AT[k][j];
             }
-            
-            
+
+
             C[i][j] = sum > EPS ? sum : 0;
         }
     }
@@ -123,7 +123,7 @@ void outputArray() {
         for (int j = 0; j < A[i].size(); j++) {
             cout << A[i][j] << " ";
         }
-        
+
         cout << '\n';
     }
 }
@@ -131,13 +131,13 @@ void outputArray() {
 //Создание минора
 void createMinor(int a, int b) {
     M.resize(A.size() - 1);
-    
+
     for (int i = 0; i < M.size(); i++) {
         M[i].resize(0);
     }
-    
+
     int k = 0;
-    
+
     for (int i = 0; i < A.size(); i++) {
         if (a != i) {
             for (int j = 0; j < A[i].size(); j++) {
@@ -145,7 +145,7 @@ void createMinor(int a, int b) {
                     M[k].push_back(A[i][j]);
                 }
             }
-            
+
             k++;
         }
     }
@@ -154,33 +154,33 @@ void createMinor(int a, int b) {
 //нахождение определителя минора
 double minorDet() {
     double det = 1;
-    
+
     for (int i = 0; i < M.size(); i++) {
         int k = i;
-        
+
         for (int j = i + 1; j < M[i].size(); j++) {
             if (abs(M[j][i]) > abs(M[k][i])) {
                 k == j;
             }
         }
-        
+
         if (abs(M[k][i]) < EPS) {
 		    det = 0;
 		    break;
 	    }
-	    
+
 	    swap(M[i], M[k]);
-	    
+
 	    if (i != k) {
 	        det = -det;
 	    }
-	    
+
 	    det *= M[i][i];
-	    
+
 	    for (int j = i + 1; j < M[i].size(); j++) {
 	        M[i][j] /= M[i][i];
 	    }
-	    
+
 	    for (int j = 0; j < M[i].size(); j++) {
 	        if (j != i && abs(M[j][i]) > EPS) {
 	            for (int k = i + 1; k < M[i].size(); k++) {
@@ -189,7 +189,7 @@ double minorDet() {
 	        }
 	    }
     }
-    
+
     return det;
 }
 
@@ -205,57 +205,57 @@ void craeteAT() {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    
+
     int n;
-    
+
     cin >> n;
-    
+
     A.resize(n);
     B.resize(n);
     C.resize(n);
     AT.resize(n);
-    
+
     for (int i = 0; i < A.size(); i++) {
         A[i].resize(n);
         B[i].resize(n);
         C[i].resize(n);
         AT[i].resize(n);
     }
-    
+
     inputArray();
-    
+
     cout << '\n';
-    
+
     outputArray();
-        
+
     cout << '\n';
-    
+
     copyArray();
-    
+
     double det = searchDet();
-    
+
     if (det) {
         craeteAT();
 
         T();
-        
+
         for (int i = 0; i < AT.size(); i++) {
             for (int j = 0; j < AT[i].size(); j++) {
                 AT[i][j] /= det;
             }
         }
-        
+
         multMatrix();
-        
+
         for (int i = 0; i < C.size(); i++) {
             for (int j = 0; j < C[i].size(); j++) {
                 cout << C[i][j] << " ";
             }
-        
+
             cout << "\n";
         }
     } else {
-        cout << "Обратной матрицы не существует!!!\n";
+        cout << "Obratnoi matrici net!!!\n";
     }
 
     return 0;
